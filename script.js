4bd5963a6355772b942844dfd94a2b63ccf8c521
Al3x-K <ola.kail1901@gmail.com>
2025-05-16 21:53:59 +0100

    ✨ Added functionality for adding new tasks

--- script.js
+++ script.js
@@ -32,10 +32,55 @@ const prepareDOMElements = () => {
 
 const prepareDOMEvents = () => {
     $addBtn.addEventListener('click', addNewTask);
+    $todoInput.addEventListener('keyup', enterCheck);
 }
 
 const addNewTask = () => {
-    console.log('ok');
+    if($todoInput.value === '') 
+    {
+        $alertInfo.innerHTML = 'Please enter a task.';
+    }
+    else 
+    {
+        $idNumber++;
+        $newTask = document.createElement('li');
+        $newTask.innerText = $todoInput.value;
+        $newTask.setAttribute('id', `todo-${$idNumber}`);
+        
+        $ulList.appendChild($newTask);
+        $todoInput.value = '';
+        $alertInfo.innerText = '';
+        createToolsArea();
+    }
 }
 
+const enterCheck = () => {
+    if (event.keyCode === 13) {
+        addNewTask();
+    }
+}
+
+const createToolsArea = () => 
+{
+    const toolsPanel = document.createElement('div');
+    toolsPanel.classList.add('tools');
+    $newTask.appendChild(toolsPanel);
+
+    const completeBtn = document.createElement('button');
+    completeBtn.classList.add('complete');
+    completeBtn.innerHTML = '<i class="fas fa-check"></i>';
+
+    const editBtn = document.createElement('button');
+    editBtn.classList.add('edit-btn');
+    editBtn.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>';
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('delete-btn');
+    deleteBtn.innerHTML = '<i class="fa-solid fa-times"></i>';
+
+    toolsPanel.appendChild(completeBtn);
+    toolsPanel.appendChild(editBtn);
+    toolsPanel.appendChild(deleteBtn);
+      
+}
 document.addEventListener('DOMContentLoaded', main);
